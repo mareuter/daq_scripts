cd0e91d71e7247a65a5777110439eaef32f2e7db
Michael Reuter <mareuternh@gmail.com>
2018-05-07 22:47:52 -0700

    Updating FITS making script.

--- python/make_fits.py
+++ python/make_fits.py
@@ -3,7 +3,8 @@ from astropy.io import fits
 import sys
 
 filehead = sys.argv[1]
-shape = (2222, 500)
+shape = (2048, 576)
+#shape = (2222, 500)
 
 toprow = []
 botrow = []
@@ -12,12 +13,16 @@ for i in range(0, 16):
     filename = "{}.{:02d}".format(filehead, i)
     print(filename)
     pixels = np.fromfile(filename, dtype=np.int32).reshape(shape)
+    pixels = pixels[:2000, 10:522]
     if i < 8:
+        pixels = pixels[::-1, ::-1]
         toprow.append(pixels)
     else:
         botrow.append(pixels)
 
+toprow.reverse()
 top = np.hstack(toprow)
+botrow.reverse()
 bot = np.hstack(botrow)
 ccd = np.vstack((bot, top))
 
