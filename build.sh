c9ea0e05313d919223fe3b7f22a12eb5dfcfc783
Michael Reuter <mareuternh@gmail.com>
2018-05-08 09:04:30 -0700

    Adding Science raft reading program.

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ g++ -o read_gds read_gds.o -Wl,-rpath=${RELEASE}/x86/lib -L${RELEASE}/x86/lib -l
 
 g++ -o read_wfs.o -c -std=gnu++0x -g -Wall -DSINGLE_TASK -I${RELEASE}/include read_wfs.cc
 g++ -o read_wfs read_wfs.o -Wl,-rpath=${RELEASE}/x86/lib -L${RELEASE}/x86/lib -ldaq_daq -ldaq_ims -ldaq_dsi
+
+g++ -o read_sci.o -c -std=gnu++0x -g -Wall -DSINGLE_TASK -I${RELEASE}/include read_sci.cc
+g++ -o read_sci read_sci.o -Wl,-rpath=${RELEASE}/x86/lib -L${RELEASE}/x86/lib -ldaq_daq -ldaq_ims -ldaq_dsi
